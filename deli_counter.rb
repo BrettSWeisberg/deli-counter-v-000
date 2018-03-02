cb71f81e26393f7aba07a37465f931eda373d334
Brett Weisberg <weisberg.brett@gmail.com>
2018-03-02 01:03:14 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -6,3 +6,14 @@ def line(katz_deli)
     puts "The line is currently empty."
   end
 end
+
+def take_a_number(katz_deli,name)
+  katz_deli.push(name)
+end
+
+def now_serving
+  puts ""
+  
+  
+end
+  
